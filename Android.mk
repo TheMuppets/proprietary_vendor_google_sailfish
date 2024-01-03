4522967e168febb18cf39aa954adb0c42672d2d4
Nolen Johnson <johnsonnolen@gmail.com>
2024-01-03 00:02:53 -0500

    sailfish: Import firmware

--- Android.mk
+++ Android.mk
@@ -6,4 +6,19 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),sailfish)
 
+$(call add-radio-file-sha1-checked,radio/aboot.img,55cac1ee9a39392c5af5efd01d3ccc47a979ed97)
+$(call add-radio-file-sha1-checked,radio/apdp.img,01220ce7e904a36f74775b490ea3e8f81b1a2e76)
+$(call add-radio-file-sha1-checked,radio/bootlocker.img,f6d44aef98dd3ef38971088b97ab0d32edb12c96)
+$(call add-radio-file-sha1-checked,radio/cmnlib32.img,a307580a4c924907a46b01c051ca6c92333a3754)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,559afd52ca3ba363c8fc404307a275af110bf960)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,0208f313666cb819c62246ddbf7bfab2e235d0ec)
+$(call add-radio-file-sha1-checked,radio/hosd.img,46282b46786ec64bc6bffef74fd47f6577cfe017)
+$(call add-radio-file-sha1-checked,radio/hyp.img,37cff5af58372d570b232fbbc7a2aefc864b8813)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,0daf9eaf0f34998c8a60360d94f717916f7a3992)
+$(call add-radio-file-sha1-checked,radio/modem.img,2f60b37abb212f31ec87ccbf13e511901f2684af)
+$(call add-radio-file-sha1-checked,radio/pmic.img,7d08353f6de560184fc89bca98d76ec0b5423133)
+$(call add-radio-file-sha1-checked,radio/rpm.img,86cd08eec869d30d68378247fee86ba068c47963)
+$(call add-radio-file-sha1-checked,radio/tz.img,e7824e879496b557fd095de9a9e4dc7cbab3aba8)
+$(call add-radio-file-sha1-checked,radio/xbl.img,7d706e3431b8d05864b1038112f62251b85bccb0)
+
 endif
